e01b9286e17a498c638f75d5caa4b80e5519240a
salman-alvi2k <102091911+salman-alvi2k@users.noreply.github.com>
2024-06-04 18:25:05 +0500

    feat: added button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      gridTemplateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav" "nav" "aside" "main"`,
+      }}
+    >
+      <GridItem area="nav" bg="gray">
+        Nav
+      </GridItem>
+      <GridItem area="aside" bg="red">
+        Aside
+      </GridItem>
+      <GridItem area="main" bg="blue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
