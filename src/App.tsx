6a6d2488882d00be29db6b9fb3a5a00503a9f72e
salman-alvi2k <102091911+salman-alvi2k@users.noreply.github.com>
2024-06-07 17:34:43 +0500

    feat(WIP): developing Navabr

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Grid, GridItem } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
@@ -8,8 +9,8 @@ function App() {
         lg: `"nav" "nav" "aside" "main"`,
       }}
     >
-      <GridItem area="nav" bg="gray">
-        Nav
+      <GridItem area="nav">
+        <Navbar />
       </GridItem>
       <GridItem area="aside" bg="red">
         Aside
