071bfc11836db1995f102fe017d512ff138a36dc
salman-alvi2k <102091911+salman-alvi2k@users.noreply.github.com>
2024-06-08 18:31:21 +0500

    feat: added dark mode button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
+import ColorSwitchMode from "./ColorSwitchMode";
 
 const Navbar = () => {
   return (
-    <HStack>
+    <HStack padding={1} margin={1} justifyContent="space-between">
       <Image src={logo} boxSize="60px" />
       <Text>Navbar</Text>
+      <ColorSwitchMode />
     </HStack>
   );
 };
