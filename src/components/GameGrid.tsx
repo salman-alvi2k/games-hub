a2616d303ebe294be9328051fffc68eeda30f85f
salman-alvi2k <102091911+salman-alvi2k@users.noreply.github.com>
2024-06-14 19:00:55 +0500

    feat(WIP): fetch game list

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,9 +1,35 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { Text } from "@chakra-ui/react";
 
+interface Game {
+  id: number;
+  name: string;
+}
+interface FetchGamesResponse {
+  count: number;
+  record: Game[];
+}
 const GameGrid = () => {
-  const [games, setGames] = useState([]);
+  const [games, setGames] = useState<Game[]>([]);
   const [errors, setErrors] = useState("");
-  return <div></div>;
+
+  useEffect(() => {
+    apiClient
+      .get<FetchGamesResponse>("/games")
+      .then((res) => setGames(res.data.record))
+      .catch((err) => setErrors(err));
+  });
+  return (
+    <>
+      {errors && <Text>{errors}</Text>}
+      <ul>
+        {games.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default GameGrid;
