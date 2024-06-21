0d6bf9ec2d522c22b8dedadb5f5769585789868f
salman-alvi2k <102091911+salman-alvi2k@users.noreply.github.com>
2024-06-21 18:43:44 +0500

    fix: fixed the syntax of the params

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
-  params: "346a4e36c6a44822b0d2f10b273196f3",
+  params: { key: "346a4e36c6a44822b0d2f10b273196f3" },
 });
