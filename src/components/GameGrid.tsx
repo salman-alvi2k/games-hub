be70e446e18a275361c307652ffeef7858d9c80e
salman-alvi2k <102091911+salman-alvi2k@users.noreply.github.com>
2024-06-21 17:27:27 +0500

    fix: console error

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
 import { Text } from "@chakra-ui/react";
+import apiClient from "../services/api-client";
 
 interface Game {
   id: number;
@@ -12,17 +12,17 @@ interface FetchGamesResponse {
 }
 const GameGrid = () => {
   const [games, setGames] = useState<Game[]>([]);
-  const [errors, setErrors] = useState("");
+  const [error, setError] = useState("");
 
   useEffect(() => {
     apiClient
       .get<FetchGamesResponse>("/games")
       .then((res) => setGames(res.data.record))
-      .catch((err) => setErrors(err));
+      .catch((err) => setError(err.messsage));
   });
   return (
     <>
-      {errors && <Text>{errors}</Text>}
+      {error && <Text>{error}</Text>}
       <ul>
         {games.map((item) => (
           <li key={item.id}>{item.name}</li>
