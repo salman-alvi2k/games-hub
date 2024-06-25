64011af80e725a1f168b11a1c356d014becd73a7
salman-alvi2k <102091911+salman-alvi2k@users.noreply.github.com>
2024-06-25 18:12:12 +0500

    fix(WIP): gamegrid component

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,4 +1,4 @@
-import { Spinner, Text } from "@chakra-ui/react";
+import { SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
@@ -8,11 +8,11 @@ const GameGrid = () => {
       {games ? (
         <>
           {error && <Text>{error}</Text>}
-          <ul className="list-none">
+          <SimpleGrid>
             {games.map((item) => (
-              <li key={item.id}>{item.name}</li>
+              <GameGrid key={item.id} />
             ))}
-          </ul>
+          </SimpleGrid>
         </>
       ) : (
         <Spinner />
