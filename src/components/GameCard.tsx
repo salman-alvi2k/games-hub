ca3d6e004dc598f6792e8ce1986df0113e6da655
salman-alvi2k <102091911+salman-alvi2k@users.noreply.github.com>
2024-07-01 18:38:27 +0500

    fix: added platform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 
 interface Props {
@@ -10,6 +10,9 @@ const GameCard = ({ game }: Props) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading>{game.name}</Heading>
+        {game.parent_platform.map(({ platform }) => (
+          <Text>{platform.name}</Text>
+        ))}
       </CardBody>
     </Card>
   );
